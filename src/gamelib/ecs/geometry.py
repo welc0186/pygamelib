a715aaf39b8d389e6e4df6332ba0a49a3da6eb41
welc0186 <49768046+welc0186@users.noreply.github.com>
2025-12-16 20:14:09 -0600

    added positionsboundscomponents and probecomponent

--- src/gamelib/ecs/geometry.py
+++ src/gamelib/ecs/geometry.py
@@ -1,5 +1,6 @@
 # Component Definitions
-from typing import Tuple
+from collections.abc import Callable
+from typing import Any, Tuple
 from dataclasses import dataclass
 from esper import Processor
 import esper
@@ -29,11 +30,34 @@ class RectComponent:
         return pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
 
 
+@dataclass
+class PositionBoundsComponent:
+    min_x: int
+    max_x: int
+    min_y: int
+    max_y: int
+    on_out_of_bounds: Callable[[int], Any]
+
+
 class MoveProcessor(Processor):
     def process(self, dt):
-        for entity, speed_comp in esper.get_component(VelocityComponent):
-            pos = esper.try_component(entity, PositionComponent)
-            if pos:
-                # TO-DO: Factor for dt
-                pos.x += speed_comp.base_speed[0]
-                pos.y += speed_comp.base_speed[1]
+        for entity, (speed_comp, pos) in esper.get_components(
+            VelocityComponent, PositionComponent
+        ):
+            # TO-DO: Factor for dt
+            pos.x += speed_comp.base_speed[0]
+            pos.y += speed_comp.base_speed[1]
+
+
+class PositionBoundsProcessor(Processor):
+    def process(self, dt):
+        for entity, (pos, bounds) in esper.get_components(
+            PositionComponent, PositionBoundsComponent
+        ):
+            if (
+                pos.x < bounds.min_x
+                or pos.x > bounds.max_x
+                or pos.y < bounds.min_y
+                or pos.y > bounds.max_y
+            ):
+                bounds.on_out_of_bounds(entity)
