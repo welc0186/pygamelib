2da3805a6577ddd2cb0ea2edbd74d7f824f0788b
Steve Welch <swelch@airgain.com>
2025-12-28 10:09:49 -0600

    rendering: added RenderSurface component and processor

--- src/gamelib/ecs/rendering.py
+++ src/gamelib/ecs/rendering.py
@@ -3,13 +3,31 @@ from dataclasses import dataclass
 from esper import Processor
 import esper
 import pygame
+from pygame import Surface
 
 from gamelib.ecs.geometry import PositionComponent
 
 
+class RenderSurfaceComponent:
+    def __init__(self, surface: Surface):
+        self.surface = surface
+
+    @classmethod
+    def from_rect(cls, rect: pygame.Rect, color: tuple = (255, 255, 255)):
+        surface = Surface((rect.width, rect.height))
+        surface.fill(color)
+        return cls(surface)
+
+    @classmethod
+    def solid_rect(cls, width: int, height: int, color: tuple = (255, 255, 255)):
+        surface = Surface((width, height))
+        surface.fill(color)
+        return cls(surface)
+
+
 @dataclass
 class RectSpriteComponent:
-    surface: pygame.Surface
+    surface: Surface
     rect: pygame.Rect
     color: tuple = (255, 255, 255)
 
@@ -24,3 +42,15 @@ class RenderRectProcessor(Processor):
 
         for entity, draw_rect in esper.get_component(RectSpriteComponent):
             pygame.draw.rect(draw_rect.surface, draw_rect.color, draw_rect.rect)
+
+
+class RenderSurfaceProcessor(Processor):
+    def __init__(self, screen: Surface):
+        super().__init__()
+        self.screen = screen
+
+    def process(self, dt):
+        for entity, (position, surface) in esper.get_components(
+            PositionComponent, RenderSurfaceComponent
+        ):
+            self.screen.blit(surface.surface, (position.x, position.y))
